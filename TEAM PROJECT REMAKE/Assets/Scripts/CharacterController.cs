db7265bf1fd5ca23c4c359a3000d13216cb3239b
BentNai1 <eattopramen@hotmail.com>
2021-02-15 23:11:51 -0500

    Initial Character Controller
    Set up a simple character controller and disabled gravity.

---
+++ TEAM PROJECT REMAKE/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    //...............................................Movement Variables
+    public float baseSpeed = 3.0f;
+    private float speedVar;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    
+
+
+    void Start()
+    {
+        //...........................................Movement Instantiation
+        speedVar = baseSpeed;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        //...........................................Movement Update
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");  
+    }
+
+    void FixedUpdate()
+    {
+        //...........................................Movement FixedUpdate
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speedVar * horizontal * Time.deltaTime;
+        position.y = position.y + speedVar * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
